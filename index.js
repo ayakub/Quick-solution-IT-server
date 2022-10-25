c7d5a2f7dc3fc0ea5585c128accc70a2e4440e12
ayakub <ayakub1086@gmail.com>
2022-10-26 02:16:33 +0600

    added all courses

--- index.js
+++ index.js
@@ -13,14 +13,7 @@ const allCourses = require('./Data/courses.json')
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-app.get('/course', (req, res) => {
-    res.send(allCourses)
-})
-app.get('/course/:id', (req, res) => {
-    const id = req.params.id;
-    const singleCourse = allCourses?.find(course => course.id == id);
-    res.send(singleCourse)
-})
+
 
 app.listen(Port, () => {
     console.log(`Example app listening on port ${Port}`)
