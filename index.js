c3fb9eb73315971c870264365c30f0fbb24de942
ayakub <ayakub1086@gmail.com>
2022-10-26 02:08:35 +0600

    install express

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors')
+const app = express()
+const Port = process.env.Port || 5000;
+
+app.use(cors());
+
+
+
+
+const allCourses = require('./Data/courses.json')
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/course', (req, res) => {
+    res.send(allCourses)
+})
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const singleCourse = allCourses?.find(course => course.id == id);
+    res.send(singleCourse)
+})
+
+app.listen(Port, () => {
+    console.log(`Example app listening on port ${Port}`)
+})
